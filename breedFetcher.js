f6024eeaffed68b2ed6074193a6517db66fb2390
Kaiya Hedley <keshedley@gmail.com>
2022-08-04 20:40:27 -0600

    breed fetcher test

---
+++ breedFetcher.js
@@ -0,0 +1,21 @@
+
+const request = require('request');
+
+const fetchBreedDescription = function(breedName, callback) {
+  const breedDomain = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+  request(breedDomain, (error, _, body) => {
+    if (error) {
+      callback(error);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data[0]) {
+      callback(null, data[0].description);
+    } else {
+      callback('Breed not found');
+    }
+  });
+
+};
+
+module.exports = { fetchBreedDescription };
